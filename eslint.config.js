29eb910ab2b9172f756380872807de8ab9095d66
wooohyerim <hyerim3414@gmail.com>
2025-10-27 16:08:45 +0900

    first commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,37 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
-
-module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
+export default {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'import',
+    'unused-imports'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
   },
-]);
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off', // React 17+
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'warn'
+  }
+};
